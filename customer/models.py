0b9aee29b50ae8a9f0b943900a81f204ae033bf8
chitranshi1234-bin <chitranshibharti3@gmail.com>
2023-06-03 14:30:44 +0530

    req

--- customer/models.py
+++ customer/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Customers(models.Model):
     first_name = models.CharField(max_length=15,null=True,blank=True)
     last_name = models.CharField(max_length=15,null=True,blank=True)
-    mobile = models.IntegerField(max_length=15,null=True,blank=True)
+    mobile = models.IntegerField(null=True,blank=True)
     address = models.TextField(null=True,blank=True)
-    age = models.IntegerField(max_length=15,null=True,blank=True)
+    age = models.IntegerField(null=True,blank=True)
     country = models.CharField(max_length=20,null=True,blank=True)
     city = models.CharField(max_length=20,null=True,blank=True)
     dob= models.DateField(max_length=15,null=True,blank=True)
@@ -20,11 +20,11 @@ class Customers(models.Model):
 class CustomerAddress(models.Model):
     customer = models.ForeignKey(Customers,on_delete=models.CASCADE,null=True,related_name='customer_address')
     street_name=models.CharField(max_length=15,null=True,blank=True)
-    street_number=models.IntegerField(max_length=15,null=True,blank=True)
+    street_number=models.IntegerField(null=True,blank=True)
     city = models.CharField(max_length=13,null=True,blank=True)
     state = models.CharField(max_length=12,null=True,blank=True)
     country = models.CharField(max_length=14,null=True,blank=True)
-    pincode = models.IntegerField(max_length=3,null=True,blank=True)
+    pincode = models.IntegerField(null=True,blank=True)
 
     
     def __str__(self):
@@ -33,7 +33,7 @@ class CustomerAddress(models.Model):
  # Create your models here.
 class CustomerAdhar(models.Model):
        customer = models.OneToOneField(Customers,on_delete=models.CASCADE)
-       adhar_number = models.IntegerField(max_length=15,null=True,blank=True)
+       adhar_number = models.IntegerField(null=True,blank=True)
        adhar_name =  models.CharField(max_length=15,null=True,blank=True)
        def __str__(self):
          return  str(self.adhar_name)
